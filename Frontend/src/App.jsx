e75922b6146eae2637259a6fe72a4a80ff1d6685
Abir Banik <abirbanik65@gmail.com>
2025-06-06 20:00:52 +0530

    add readme

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -10,9 +10,7 @@ import './App.css'
 
 function App() {
  
-  const [ code, setCode ] = useState(` function sum() {
-  return 1 + 1
-}`)
+  const [ code, setCode ] = useState(``)
 
   const [ review, setReview ] = useState(``)
 
